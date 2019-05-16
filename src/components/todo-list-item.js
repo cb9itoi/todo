289e61531bb9284b1c41cba8088d42f89fa0a704
Maksim Kostishin <maksim@kostishin.com>
2019-05-16 16:46:31 +0300

    Добавил коментарий

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -15,7 +15,7 @@ const TodoListItem = ({ label, important = false }) => {
           className="todo-list-item-label"
           style={style}>
         {label}
-      </span>
+      </span> {/* Проверка */}
 
       <button type="button"
               className="btn btn-outline-success btn-sm float-right">
